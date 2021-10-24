3163759ab235f32181daf4a8f29fb686e24213e3
Sirisha Munukutla <smunukutla@verticalrelevance.com>
2021-10-24 00:46:43 -0700

    Added speech synthesizer

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,56 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+
+  var speech = window.speechSynthesis;
+
+  var voiceDropdown = document.querySelector('#voice-select');
+  var voices = [];
+
+  window.speechSynthesis.onvoiceschanged = function() {
+    voices = speech.getVoices();
+    for (var i = 0; i < voices.length; i++) {
+      var option = document.createElement('option');
+      option.textContent = voices[i].name;
+      option.setAttribute('lang', voices[i].lang);
+      option.setAttribute('name', voices[i].name);
+      option.setAttribute('voiceNum', i);
+      voiceDropdown.appendChild(option);
+    }
+  }
+
+  let voiceOption = 0;
+
+  voiceDropdown.addEventListener('change', function() {
+    let selectedVoice = voiceDropdown.value;
+    for (var i = 0; i < voices.length; i++) {
+      if (voices[i].name == selectedVoice) {
+        voiceOption = voices[i];
+      }
+    }
+    //console.log(voiceOption.name);
+  });
+
+  let words = "";
+
+  let textBox = document.querySelector('#text-to-speak');
+  textBox.addEventListener('change', function() {
+    words = textBox.value;
+  });
+
+  let face = document.querySelector('img');
+
+  let button = document.querySelector('button');
+  button.addEventListener('click', function() {
+    var toSpeak = new SpeechSynthesisUtterance(words);
+    toSpeak.voice = voiceOption;
+    speech.speak(toSpeak);
+    /**while (speech.speaking) {
+      face.setAttribute('src', 'assets/images/smiling-open.png');
+    }
+    face.setAttribute('src', 'smiling.png');**/
+  })
+
+  
+
 }
