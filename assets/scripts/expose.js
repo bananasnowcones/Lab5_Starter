ceaed9389c4ce9e230fcf8bb318397e57343434e
bananasnowcones <43969773+bananasnowcones@users.noreply.github.com>
2021-10-23 19:23:14 -0700

    added skeleton + button stuff

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,25 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+ /*
+  var img = document.createElement('img');
+  img.src = 'air-horn.svg';
+  var audio = document.createElement('audio');
+  audio.src = 'sit=horn-mp3';
+*/
+
+//1. Add event listener function for when drop down is clicked
+//2. inside we can make sure the image displays and audio sets
+
+//3. Add event listener function for volume slider
+//4. Specify volume icon
+
+//5. Play sound event listener function
+//6. Sound plays, gif plays
+
+
+let button1 = document.querySelector('#horn-selector');
+button1.addEventListener('click', function() {
+  alert('hi');
+})
 }
