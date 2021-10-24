9cbd21122fb500bff3b4b1359cee9e655947e380
Sirisha Munukutla <smunukutla@verticalrelevance.com>
2021-10-23 23:50:02 -0700

    Added volume level if statements

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -44,6 +44,29 @@ function init() {
     
   });
 
+  var volume = document.querySelector('#volume-controls');
+  var volImg = document.querySelector('img[alt="Volume level 2"]');
+  volume.addEventListener('change', function() {
+    let volumeLevel = volume.value;
+    console.log(volume.value);
+    if (volumeLevel == 0) {
+      volImg.setAttribute('src', 'assets/icons/volume-level-0.svg');
+      audio.volume = 0;
+    } else if (volumeLevel < 33) {
+      volImg.setAttribute('src', 'assets/icons/volume-level-1.svg');
+      audio.volume = volumeLevel/100;
+    } else if (volumeLevel < 67) {
+      volImg.setAttribute('src', 'assets/icons/volume-level-2.svg');
+      audio.volume = volumeLevel/100;
+    } else {
+      volImg.setAttribute('src', 'assets/icons/volume-level-3.svg');
+      audio.volume = volumeLevel/100;
+      if (volumeLevel > 100) {
+        audio.volume = 1;
+      }
+    }
+  });
+
   let button = document.querySelector('button');
   button.addEventListener('click', function() {
     audio.play();
