37f78e21f207d1e446ce10162936f6428cd55098
Michael Yap <43969773+bananasnowcones@users.noreply.github.com>
2021-10-23 23:52:39 -0700

    gif added

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,23 +1,9 @@
 // expose.js
+const jsConfetti = new JSConfetti();
 
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  /*
-    var img = document.createElement('img');
-    img.src = 'air-horn.svg';
-    var audio = document.createElement('audio');
-    audio.src = 'sit=horn-mp3';
-  */
-
-  //1. Add event listener function for when drop down is clicked
-  //2. inside we can make sure the image displays and audio sets
-
-  //3. Add event listener function for volume slider
-  //4. Specify volume icon
-
-  //5. Play sound event listener function
-  //6. Sound plays, gif plays
 
   var audio = document.querySelector('audio');
 
@@ -33,6 +19,7 @@ function init() {
     else if (hornChoice == "party-horn") {
       hornImage.setAttribute('src', 'assets/images/party-horn.svg');
       audio.setAttribute('src', 'assets/audio/party-horn.mp3');
+      jsConfetti.addConfetti();
     }
     else if (hornChoice == "car-horn") {
       hornImage.setAttribute('src', 'assets/images/car-horn.svg');
